ea9071adaa7315ac77eca3059c07b69b1393b4d5
guillerminalatorre <44948061+guillerminalatorre@users.noreply.github.com>
2020-06-08 20:50:11 -0300

    pantalla.menu

--- src/tuto.py
+++ src/tuto.py
@@ -5,24 +5,26 @@ pygame.init()
 pantalla = pygame.display.set_mode((280, 550))
 reloj = pygame.time.Clock()
 hecho = False
- 
+imagen = pygame.image.load("menu.png")
 
 font = pygame.font.SysFont("quando", 50)
 pygame.display.set_caption("Harry Potter Quidditch")
 text = font.render("Hola, Mundo", True, (0, 139, 139))
- 
+
 while not hecho:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             hecho = True
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             hecho = True
- 
+
     pantalla.fill((255, 255, 255))
-    pantalla.blit(text,((280 - text.get_width()) // 2, (550 - text.get_height()) // 3))
+    pantalla.blit(text, ((280 - text.get_width()) // 2, (550 - text.get_height()) // 3))
+    pantalla.blit(imagen, (0, 0))
+    pygame.display.update()
 
     pygame.display.flip()
     reloj.tick(60)
-    
-    
+
+
    
