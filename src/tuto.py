3542c59142951fa384f492cce553e1101913eccc
guillerminalatorre <44948061+guillerminalatorre@users.noreply.github.com>
2020-06-19 02:02:33 -0300

    19/6  spring 1

--- src/tuto.py
+++ src/tuto.py
@@ -11,6 +11,14 @@ font = pygame.font.SysFont("quando", 50)
 pygame.display.set_caption("Harry Potter Quidditch")
 text = font.render("Hola, Mundo", True, (0, 139, 139))
 
+def dibujar_botones(lista_botones):
+    for boton in lista_botones:
+        if boton['on_click']:
+            pantalla.blit(boton['imagen_pressed'], boton['rect'])
+        else:
+            pantalla.blit(boton['imagen'], boton['rect'])
+
+
 while not hecho:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
